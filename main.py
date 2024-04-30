b8dd3986f28dd1629d53d89bbb3a78958bd719e2
shylusha1 <dimkofgyuj@gmail.com>
2024-04-30 17:59:45 +0300

    del tg token

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.filters.command import Command
 import os
 import yt_dlp
 
-bot = Bot(token="tok")
+bot = Bot(token="7186100215:AAGFfQ_L2odWQRgP5kHFhiDUWDS088nCrdU")
 
 dp = Dispatcher()
 
@@ -17,19 +17,20 @@ async def process_start_command(message: types.Message):
 @dp.message()
 async def echo(message: types.Message):
     try:
-        with yt_dlp.YoutubeDL({'outtmpl': '%(id)s.%(ext)s', 'format': 'best'}) as ydl:
+        with yt_dlp.YoutubeDL({'outtmpl': '%(id)s.%(ext)s',
+                               'format': 'best',
+                               'merge_output_format': 'mp4'
+                               }) as ydl:
             infoid = ydl.extract_info(message.text, download=True)['id']
-            infoex = ydl.extract_info(message.text, download=False)['ext']
-            print(infoex)
         msg = await message.reply_document(
             document=types.FSInputFile(
-                path=f'{infoid}.{infoex}',
+                path=f'{infoid}.mp4',
             ),
         )
         await bot.send_message(message.chat.id, "Ваше видео", reply_to_message_id=msg.message_id)
-        os.remove(f'{infoid}.{infoex}')
+        os.remove(f'{infoid}.mp4')
     except:
-        await bot.send_message(message.chat.id, "error")
+        await bot.send_message(message.chat.id, "Ссылка некорректна, попробуйте ещё раз")
 
 
 async def main():
